e3c9598251bab9c6152a737c3085eb0ea3c8cf42
anit172737 <anitdhadve17@gmail.com>
2025-12-10 15:42:56 +0530

    meta pixel added into layout file under head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,35 @@ export default function RootLayout({
       <GoogleTagManager gtmId="GTM-N668R594" />
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* Meta Pixel Code */}
+        <script>
+          {`
+  !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '26114217091514269');
+fbq('track', 'PageView');
+  `}
+        </script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=26114217091514269&ev=PageView&noscript=1"
+            alt="meta pixel"
+          />
+        </noscript>
       </head>
       <body className={inter.className}>
-        <PasswordProtection>
-          <ToastProvider>{children}</ToastProvider>
-        </PasswordProtection>
+        {/* <PasswordProtection> */}
+        <ToastProvider>{children}</ToastProvider>
+        {/* </PasswordProtection> */}
       </body>
     </html>
   );
