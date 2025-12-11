7c23f88ea7c86c4e178316aa20c302b36f006bf4
anit172737 <anitdhadve17@gmail.com>
2025-12-11 16:01:01 +0530

    script added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -23,6 +24,41 @@ export default function RootLayout({
       <GoogleTagManager gtmId="GTM-N668R594" />
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* Yahoo Conversion Tracking Script */}
+        <Script
+          id="yahoo-conversion-tracking"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,t,r,u){
+                w[u]=w[u]||[];
+                w[u].push({
+                  'projectId':'10000',
+                  'properties':{
+                    'pixelId':'10210659',
+                    'he': '<email_address>',
+                    'hph': '<phone_number>'
+                  }
+                });
+                var s=d.createElement(t);
+                s.src=r;
+                s.async=true;
+                s.onload=s.onreadystatechange=function(){
+                  var y,rs=this.readyState,c=w[u];
+                  if(rs&&rs!="complete"&&rs!="loaded"){return}
+                  try{
+                    y=YAHOO.ywa.I13N.fireBeacon;
+                    w[u]=[];
+                    w[u].push=function(p){y([p])};
+                    y(c)
+                  }catch(e){}
+                };
+                var scr=d.getElementsByTagName(t)[0],par=scr.parentNode;
+                par.insertBefore(s,scr)
+              })(window,document,"script","https://s.yimg.com/wi/ytc.js");
+            `,
+          }}
+        />
         {/* Meta Pixel Code */}
         <script>
           {`
